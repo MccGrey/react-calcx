463fd121199de35b16e47966bf02983d0c25877d
MccGrey <chuxzzie01@yahoo.com>
2022-09-27 07:57:30 +0100

    add state for input display and output display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useReducer } from "react";
 import "./style.css";
 
+function reducer(state, action) {}
+
 function App() {
+  const [currentOperand, previousOperand, operation] = useReducer(reducer, {});
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">77744</div>
-        <div className="current-operand">445322566</div>
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
